96a6b8597ff7e32b2e27802284c7226cf9685faa
Tuğrul Yıldırım <yazilimcitugrul@gmail.com>
2026-06-01 17:30:58 +0300

    Add rewardUsage, orderInquiry, build3DOOSForm, pay3DModel and update docs

--- src/Services/GarantiPosService.php
+++ src/Services/GarantiPosService.php
@@ -334,7 +334,7 @@ class GarantiPosService
             'errorurl' => $errorUrl,
             'customeripaddress' => $orderData['ip_address'] ?? request()->ip(),
             'customeremailaddress' => $orderData['email'] ?? '',
-            'secure3dsecuritylevel' => '3D',
+            'secure3dsecuritylevel' => $orderData['security_level'] ?? '3D_PAY',
             'secure3dhash' => $hashData,
             'cardnumber' => $cardData['number'],
             'cardexpiredatemonth' => $cardData['expire_month'],
@@ -350,5 +350,235 @@ class GarantiPosService
         $html .= '<script>document.getElementById("garanti-3d-form").submit();</script>';
 
         return $html;
+    }
+
+    /**
+     * Point Usage (Puan Kullanımı)
+     *
+     * @param array $orderData
+     * @param array $cardData
+     * @param string $pointAmount
+     * @return array
+     * @throws GarantiPosException
+     */
+    public function rewardUsage(array $orderData, array $cardData, string $pointAmount): array
+    {
+        $securityData = HashGenerator::generateSecurityData(
+            $this->config['prov_password'],
+            $this->config['terminal_id']
+        );
+
+        $hashData = HashGenerator::generateHashData(
+            $orderData['order_id'],
+            $this->config['terminal_id'],
+            $cardData['number'],
+            $pointAmount,
+            $securityData
+        );
+
+        $payload = [
+            'Mode' => $this->config['mode'],
+            'Version' => 'v0.01',
+            'Terminal' => [
+                'ProvUserID' => $this->config['prov_user_id'],
+                'HashData' => $hashData,
+                'UserID' => $this->config['prov_user_id'],
+                'ID' => $this->config['terminal_id'],
+                'MerchantID' => $this->config['merchant_id'],
+            ],
+            'Customer' => [
+                'IPAddress' => $orderData['ip_address'] ?? request()->ip(),
+                'EmailAddress' => $orderData['email'] ?? '',
+            ],
+            'Card' => [
+                'Number' => $cardData['number'],
+                'ExpireDate' => $cardData['expire_month'] . $cardData['expire_year'],
+                'CVV2' => $cardData['cvv'],
+            ],
+            'Order' => [
+                'OrderID' => $orderData['order_id'],
+            ],
+            'Transaction' => [
+                'Type' => 'rewardusage',
+                'Amount' => $pointAmount,
+                'CurrencyCode' => $this->config['currency'],
+            ]
+        ];
+
+        return $this->sendRequest($payload);
+    }
+
+    /**
+     * Order Inquiry (Sipariş Sorgulama)
+     *
+     * @param string $orderId
+     * @return array
+     * @throws GarantiPosException
+     */
+    public function orderInquiry(string $orderId): array
+    {
+        $securityData = HashGenerator::generateSecurityData(
+            $this->config['prov_password'],
+            $this->config['terminal_id']
+        );
+
+        $hashData = HashGenerator::generateHashData(
+            $orderId,
+            $this->config['terminal_id'],
+            '',
+            '1', // Dummy amount
+            $securityData
+        );
+
+        $payload = [
+            'Mode' => $this->config['mode'],
+            'Version' => 'v0.01',
+            'Terminal' => [
+                'ProvUserID' => $this->config['prov_user_id'],
+                'HashData' => $hashData,
+                'UserID' => $this->config['prov_user_id'],
+                'ID' => $this->config['terminal_id'],
+                'MerchantID' => $this->config['merchant_id'],
+            ],
+            'Order' => [
+                'OrderID' => $orderId,
+            ],
+            'Transaction' => [
+                'Type' => 'orderinq',
+                'Amount' => '1',
+                'CurrencyCode' => $this->config['currency'],
+            ]
+        ];
+
+        return $this->sendRequest($payload);
+    }
+
+    /**
+     * Generate 3D Secure Ortak Ödeme Sayfası (OOS) Form HTML
+     *
+     * @param array $orderData
+     * @param string $successUrl
+     * @param string $errorUrl
+     * @param string $type
+     * @return string
+     */
+    public function build3DOOSForm(array $orderData, string $successUrl, string $errorUrl, string $type = 'sales'): string
+    {
+        $securityData = HashGenerator::generateSecurityData(
+            $this->config['prov_password'],
+            $this->config['terminal_id']
+        );
+
+        $hashData = HashGenerator::generate3DHash(
+            $this->config['terminal_id'],
+            $orderData['order_id'],
+            $orderData['amount'],
+            $successUrl,
+            $errorUrl,
+            $type,
+            $orderData['installment'] ?? '',
+            $this->config['store_key'],
+            $securityData
+        );
+
+        $endpoint = $this->config['mode'] === 'PROD'
+            ? 'https://sanalposprov.garanti.com.tr/servlet/gt3dengine'
+            : 'https://sanalposprovtest.garanti.com.tr/servlet/gt3dengine';
+
+        $formInputs = [
+            'mode' => $this->config['mode'],
+            'apiversion' => 'v0.01',
+            'terminalprovuserid' => $this->config['prov_user_id'],
+            'terminaluserid' => $this->config['prov_user_id'],
+            'terminalmerchantid' => $this->config['merchant_id'],
+            'txntype' => $type,
+            'txnamount' => $orderData['amount'],
+            'txncurrencycode' => $this->config['currency'],
+            'txninstallmentcount' => $orderData['installment'] ?? '',
+            'orderid' => $orderData['order_id'],
+            'terminalid' => $this->config['terminal_id'],
+            'successurl' => $successUrl,
+            'errorurl' => $errorUrl,
+            'customeripaddress' => $orderData['ip_address'] ?? request()->ip(),
+            'customeremailaddress' => $orderData['email'] ?? '',
+            'secure3dsecuritylevel' => '3D_OOS_PAY',
+            'secure3dhash' => $hashData,
+        ];
+
+        $html = '<form id="garanti-3d-oos-form" action="'.$endpoint.'" method="post">';
+        foreach ($formInputs as $key => $value) {
+            $html .= '<input type="hidden" name="'.$key.'" value="'.htmlspecialchars($value).'">';
+        }
+        $html .= '</form>';
+        $html .= '<script>document.getElementById("garanti-3d-oos-form").submit();</script>';
+
+        return $html;
+    }
+
+    /**
+     * Pay 3D Model Second Step (Otorizasyon)
+     *
+     * @param array $orderData
+     * @param array $cardData
+     * @param array $threeDResponse POST data from Garanti 3D success redirect
+     * @return array
+     * @throws GarantiPosException
+     */
+    public function pay3DModel(array $orderData, array $cardData, array $threeDResponse): array
+    {
+        $securityData = HashGenerator::generateSecurityData(
+            $this->config['prov_password'],
+            $this->config['terminal_id']
+        );
+
+        $hashData = HashGenerator::generateHashData(
+            $orderData['order_id'],
+            $this->config['terminal_id'],
+            $cardData['number'],
+            $orderData['amount'],
+            $securityData
+        );
+
+        $payload = [
+            'Mode' => $this->config['mode'],
+            'Version' => 'v0.01',
+            'Terminal' => [
+                'ProvUserID' => $this->config['prov_user_id'],
+                'HashData' => $hashData,
+                'UserID' => $this->config['prov_user_id'],
+                'ID' => $this->config['terminal_id'],
+                'MerchantID' => $this->config['merchant_id'],
+            ],
+            'Customer' => [
+                'IPAddress' => $orderData['ip_address'] ?? request()->ip(),
+                'EmailAddress' => $orderData['email'] ?? '',
+            ],
+            'Card' => [
+                'Number' => $cardData['number'],
+                'ExpireDate' => $cardData['expire_month'] . $cardData['expire_year'],
+                'CVV2' => $cardData['cvv'],
+            ],
+            'Order' => [
+                'OrderID' => $orderData['order_id'],
+                'GroupID' => '',
+                'Description' => $orderData['description'] ?? '',
+            ],
+            'Transaction' => [
+                'Type' => 'sales',
+                'InstallmentCnt' => $orderData['installment'] ?? '',
+                'Amount' => $orderData['amount'],
+                'CurrencyCode' => $this->config['currency'],
+                'CardholderPresentCode' => '13',
+                'MotoInd' => 'N',
+                'Secure3D' => [
+                    'AuthenticationCode' => $threeDResponse['cavv'] ?? '',
+                    'SecurityLevel' => $threeDResponse['eci'] ?? '',
+                    'TxnID' => $threeDResponse['xid'] ?? '',
+                    'Md' => $threeDResponse['md'] ?? '',
+                ]
+            ]
+        ];
+
+        return $this->sendRequest($payload);
     }
 }
